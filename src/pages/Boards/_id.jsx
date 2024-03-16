d2176ad9145bea2dd4b17b1726a64c508a90aaaf
namlato102 <namdang2798@gmail.com>
2024-03-16 22:22:12 +0100

    Refactor Project Structur

---
+++ src/pages/Boards/_id.jsx
@@ -0,0 +1,24 @@
+import Container from '@mui/material/Container'
+
+import AppBar from '../../components/AppBar'
+import BoardBar from './BoardBar'
+import BoardContent from './BoardContent'
+
+function Board() {
+  return (
+    <>
+      <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
+        {/* App Bar */}
+        <AppBar />
+
+        {/* Board Bar */}
+        <BoardBar />
+
+        {/* Board Content */}
+        <BoardContent />
+      </Container>
+    </>
+  )
+}
+
+export default Board
---
+++ src/utils/sorts.js
@@ -0,0 +1,1 @@
+// sort columns, sort rows, sort cards
