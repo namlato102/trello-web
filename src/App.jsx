08c68d0b36d11e3698c0c5567dd4adb18aed2f42
namlato102 <namdang2798@gmail.com>
2024-04-14 18:07:14 +0200

    add boards route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import Board from '~/pages/Boards/_id'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       {/*  React Router Dom /boards/{board_id} */}
+      <div>
+        <Routes>
+          {/* Board Details */}
+          <Route path="/boards" element={<Board />} />
+          {/* <Route path="/about" element={<About />} /> */}
+        </Routes>
+      </div>
 
-      {/* Board Details */}
-      <Board />
-    </>
+    </BrowserRouter>
   )
 }
 
