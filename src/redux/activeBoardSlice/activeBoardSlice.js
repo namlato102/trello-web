615facfe983307b6a74d185325a88a21a0bf8ed0
namlato102 <dinhnam.dang98@gmail.com>
2024-05-14 19:25:19 +0200

    [added] config redux

---
+++ src/redux/activeBoardSlice/activeBoardSlice.js
@@ -0,0 +1,78 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
+import { API_URL } from '~/utils/constants'
+import { mapOrder } from '~/utils/sorts'
+import { isEmpty } from 'lodash'
+import { generatePlaceholderCard } from '~/utils/formatters'
+
+// Khởi tạo giá trị State của một cái Slice trong Redux
+const initialState = {
+  currentActiveBoard: null
+}
+
+// Các hành động gọi api (bất đồng bộ) và cập nhật dữ liệu vào Redux, dùng Middleware createAsyncThunk đi kèm với extraReducers
+// https://redux-toolkit.js.org/api/createAsyncThunk
+export const fetchBoardDetailsAPI = createAsyncThunk(
+  'activeBoard/fetchBoardDetailsAPI',
+  async (boardId) => {
+    const response = await axios.get(`${API_URL}/v1/boards/${boardId}`)
+    // Lưu ý: axios sẽ trả kết quả về qua property của nó là data
+    return response.data
+  }
+)
+
+// Khởi tạo một cái Slice trong kho lưu trữ - Redux Store
+export const activeBoardSlice = createSlice({
+  name: 'activeBoard',
+  initialState,
+  // Reducers: Nơi xử lý dữ liệu đồng bộ
+  reducers: {
+    updateCurrentActiveBoard: (state, action) => {
+      // action.payload là chuẩn đặt tên khi nhận dữ liệu vào reducer, chúng ta sẽ gán nó ra một biến có nghĩa hơn
+      const board = action.payload
+
+      // Xử lý dữ liệu nếu cần thiết...
+
+      // Update lại dữ liệu của cái currentActiveBoard
+      state.currentActiveBoard = board
+    }
+  },
+  // extraReducer: Nới xử lý dữ liệu bất đồng bộ
+  extraReducers: (builder) => {
+    builder.addCase(fetchBoardDetailsAPI.fulfilled, (state, action) => {
+      // action.payload ở đây chính là cái response.data trả về ở cái middleeware createAsyncThunk phía bên trên
+      const board = action.payload
+
+      // Xử lý dữ liệu nếu cần thiết...
+      // Sắp xếp thứ tự các column luôn ở đây trước khi đưa dữ liệu xuống bên dưới các component con (video 71 đã giải thích lý do ở phần Fix bug quan trọng)
+      board.columns = mapOrder(board.columns, board.columnOrderIds, '_id')
+
+      board.columns.forEach(column => {
+        // Khi f5 trang web thì cần xử lý vấn đề kéo thả vào một column rỗng (Nhớ lại video 37.2, code hiện tại là video 69)
+        if (isEmpty(column.cards)) {
+          column.cards = [generatePlaceholderCard(column)]
+          column.cardOrderIds = [generatePlaceholderCard(column)._id]
+        } else {
+          // Sắp xếp thứ tự các cards luôn ở đây trước khi đưa dữ liệu xuống bên dưới các component con (video 71 đã giải thích lý do ở phần Fix bug quan trọng)
+          column.cards = mapOrder(column.cards, column.cardOrderIds, '_id')
+        }
+      })
+
+      // Update lại dữ liệu của cái currentActiveBoard
+      state.currentActiveBoard = board
+    })
+  }
+})
+
+// Action creators are generated for each case reducer function
+// Actions: Là nơi dành cho các components bên dưới gọi bằng dispatch() tới nó để cập nhật lại dữ liệu thông qua reducer (chạy đồng bộ)
+// Để ý ở trên thì không thấy properties actions đâu cả, bởi vì những cái actions này đơn giản là được thằng redux tạo tự động theo tên của reducer nhé.
+export const { updateCurrentActiveBoard } = activeBoardSlice.actions
+
+// Selectors: Là nơi dành cho các components bên dưới gọi bằng hook useSelector() để lấy dữ liệu từ trong kho redux store ra sử dụng
+export const selectCurrentActiveBoard = (state) => {
+  return state.activeBoard.currentActiveBoard
+}
+
+// export default activeBoardSlice.reducer
+export const activeBoardReducer = activeBoardSlice.reducer
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,9 @@
 // Redux: State management tool
+import { activeBoardReducer } from './activeBoardSlice/activeBoardSlice'
+import { configureStore } from '@reduxjs/toolkit'
+
+export const store = configureStore({
+  reducer: {
+    activeBoard: activeBoardReducer
+  }
+})
