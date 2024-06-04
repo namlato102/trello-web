cb34c070584226b3aca40762ba43e34dd6ff78da
namlato102 <namdang2798@gmail.com>
2024-06-04 23:55:17 +0200

    [added] verify account and login with jwt token

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 // Redux: State management tool
 import { activeBoardReducer } from './activeBoard/activeBoardSlice'
 import { configureStore } from '@reduxjs/toolkit'
+import { userReducer } from './user/userSlice'
 
 export const store = configureStore({
   reducer: {
-    activeBoard: activeBoardReducer
+    activeBoard: activeBoardReducer,
+    user: userReducer
   }
 })
