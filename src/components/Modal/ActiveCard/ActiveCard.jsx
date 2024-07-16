a2d10f3e20254aea51a2c2fe69b49162a612278a
namlato102 <namdang2798@gmail.com>
2024-07-16 18:26:25 +0200

    [added] add/remove another user to/from active card when clicked on user avatar and add/remove current user when clicked on join/leave button.

--- src/components/Modal/ActiveCard/ActiveCard.jsx
+++ src/components/Modal/ActiveCard/ActiveCard.jsx
@@ -40,6 +40,9 @@ import {
 } from '~/redux/activeCard/activeCardSlice'
 import { updateCardDetailsAPI } from '~/apis'
 import { updateCardInBoard } from '~/redux/activeBoard/activeBoardSlice'
+import { selectCurrentUser } from '~/redux/user/userSlice'
+import { CARD_MEMBER_ACTIONS } from '~/utils/constants'
+import ExitToAppIcon from '@mui/icons-material/ExitToApp'
 
 
 import { styled } from '@mui/material/styles'
@@ -68,6 +71,7 @@ function ActiveCard() {
   const dispatch = useDispatch()
   const activeCard = useSelector(selectCurrentActiveCard)
   const isShowModalActiveCard = useSelector(selectIsShowModalActiveCard)
+  const currentUser = useSelector(selectCurrentUser)
 
   const handleCloseModal = () => {
     dispatch(clearAndHideCurrentActiveCard())
@@ -106,6 +110,10 @@ function ActiveCard() {
 
   const onAddCardComment = async (commentToAdd) => {
     await callApiUpdateCard({ commentToAdd })
+  }
+
+  const onUpdateCardMembers = (incomingMemberInfo) => {
+    callApiUpdateCard({ incomingMemberInfo })
   }
 
   return (
@@ -163,7 +171,10 @@ function ActiveCard() {
               <Typography sx={{ fontWeight: '600', color: 'primary.main', mb: 1 }}>Members</Typography>
 
               {/* Feature 02: Card's members */}
-              <CardUserGroup />
+              <CardUserGroup
+                cardMemberIds={activeCard?.memberIds}
+                onUpdateCardMembers={onUpdateCardMembers}
+              />
             </Box>
 
             <Box sx={{ mb: 3 }}>
@@ -198,10 +209,30 @@ function ActiveCard() {
             <Typography sx={{ fontWeight: '600', color: 'primary.main', mb: 1 }}>Add To Card</Typography>
             <Stack direction="column" spacing={1}>
               {/* Feature 05: add user to active card */}
-              <SidebarItem className="active">
-                <PersonOutlineOutlinedIcon fontSize="small" />
-                Join
-              </SidebarItem>
+              {/* Nếu user hiện tại đang đăng nhập chưa thuộc mảng memberIds của card thì mới cho hiện nút Join ra */}
+              {/* Khi Click vào Join thì nó sẽ luôn là hành động ADD */}
+              {activeCard?.memberIds?.includes(currentUser._id)
+                ?
+                <SidebarItem
+                  className="active"
+                  onClick={() => onUpdateCardMembers({
+                    userId: currentUser._id,
+                    action: CARD_MEMBER_ACTIONS.REMOVE
+                  })}
+                >
+                  <ExitToAppIcon fontSize="small" /> Leave
+                </SidebarItem>
+                :
+                <SidebarItem
+                  className="active"
+                  onClick={() => onUpdateCardMembers({
+                    userId: currentUser._id,
+                    action: CARD_MEMBER_ACTIONS.ADD
+                  })}
+                >
+                  <PersonOutlineOutlinedIcon fontSize="small" /> Join
+                </SidebarItem>
+              }
               {/* Feature 06: update card cover */}
               <SidebarItem className="active" component="label">
                 <ImageOutlinedIcon fontSize="small" />
